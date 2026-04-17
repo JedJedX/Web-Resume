eeb1cbf7a963b7fcb186db5c964f7f0be246929c
Jedsada <s65122250025@ssru.ac.th>
2026-04-17 11:19:37 +0700

    First commit: Jedsada Web Resume

---
+++ dist/app.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const myResume = {
+    personal: {
+        name: "Jedsada Chimnok",
+        position: "Frontend Developer",
+        email: "jedsada.omsin@gmail.com",
+        phone: "063-516-3985",
+        location: "Bang Phlat, Bangkok",
+        about: "I am eager to develop my skills in UX Research, Wireframing, and Prototyping..."
+    },
+    education: {
+        university: "Suan Sunandha Rajabhat University",
+        major: "Computer Science",
+        gpa: 3.56
+    },
+    skills: {
+        programming: ["Java", "Python", "JavaScript", "TypeScript"],
+        frontend: ["React.js", "HTML", "CSS"],
+        tools: ["Git/GitHub", "Figma", "VS Code"]
+    }
+};
